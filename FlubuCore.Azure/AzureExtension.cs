fea93fdecfc2a2a0c897b40d62df1898fa4a8968
markoz <mzorec@gmail.com>
2018-10-10 10:10:42 +0200

    Adds azure flubu exntesion

---
+++ FlubuCore.Azure/AzureExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlubuCore.Context.FluentInterface.Interfaces;
+
+namespace FlubuCore.Azure
+{
+    public static class AzureExtension
+    {
+        /// <summary>
+        /// Azure specific tasks.
+        /// </summary>
+        /// <param name="flubu"></param>
+        /// <returns></returns>
+        public static Azure Azure(this ITaskFluentInterface flubu)
+        {
+            return new Azure();
+        }
+    }
+}
